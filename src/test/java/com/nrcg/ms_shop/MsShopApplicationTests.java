b50799aa4dbcdadff45acc38db633db5de43fa7e
Nestor Castillo <nestorraulcgomez@gmail.com>
2024-03-23 00:05:27 -0300

    first commit

---
+++ src/test/java/com/nrcg/ms_shop/MsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrcg.ms_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
